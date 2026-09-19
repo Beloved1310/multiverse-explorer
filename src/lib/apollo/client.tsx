d1470f8f49547200374021cf9e94b2ed5a1f63cf
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-09-19 15:58:44 +0100

    feat: add error and retry links to apollo link chain

--- src/lib/apollo/client.tsx
+++ src/lib/apollo/client.tsx
@@ -1,20 +1,50 @@
 "use client";
 
-import { HttpLink } from "@apollo/client";
+import { ApolloLink, HttpLink } from "@apollo/client";
+import { CombinedGraphQLErrors } from "@apollo/client/errors";
+import { ErrorLink } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
 import {
   ApolloClient,
   ApolloNextAppProvider,
   InMemoryCache,
 } from "@apollo/client-integration-nextjs";
+import { isRetryableNetworkError } from "./retry-condition";
 
 const GRAPHQL_ENDPOINT =
   process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
   "https://rickandmortyapi.com/graphql";
 
+const errorLink = new ErrorLink(({ error, operation }) => {
+  if (process.env.NODE_ENV !== "development") return;
+
+  const errorType = CombinedGraphQLErrors.is(error)
+    ? "GraphQL error"
+    : "Network error";
+
+  console.error(`[${errorType}] ${operation.operationName}`, {
+    variables: operation.variables,
+    error,
+  });
+});
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 300,
+    jitter: true,
+  },
+  attempts: {
+    max: 3,
+    retryIf: isRetryableNetworkError,
+  },
+});
+
 function makeClient() {
+  const httpLink = new HttpLink({ uri: GRAPHQL_ENDPOINT });
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({ uri: GRAPHQL_ENDPOINT }),
+    link: ApolloLink.from([errorLink, retryLink, httpLink]),
   });
 }
 
