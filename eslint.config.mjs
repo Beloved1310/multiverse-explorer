5065533697e33b4a36af2d93db5655a037fd4d60
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-09-19 14:14:16 +0100

    feat: configure Prettier with Tailwind class sorting, deferring to ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Must come last: turns off stylistic ESLint rules that would otherwise
+  // fight with Prettier's formatting.
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
