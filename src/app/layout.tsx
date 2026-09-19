195f0de05e5f33b564a3e9a4b2525c99e625fc47
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-09-19 14:46:59 +0100

    feat: replace single Apollo instance with the official Next.js App Router provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
+import { ApolloWrapper } from "@/lib/apollo/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
-        <Providers>{children}</Providers>
+        <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
   );
---
+++ src/lib/apollo/client.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloNextAppProvider,
+  InMemoryCache,
+} from "@apollo/client-integration-nextjs";
+
+const GRAPHQL_ENDPOINT =
+  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
+  "https://rickandmortyapi.com/graphql";
+
+function makeClient() {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({ uri: GRAPHQL_ENDPOINT }),
+  });
+}
+
+export function ApolloWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
