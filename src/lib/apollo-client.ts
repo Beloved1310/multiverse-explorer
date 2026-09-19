5643d2171980eeb99a4e92143c328a1e42a6ff68
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-09-19 13:52:58 +0100

    feat: wire up Apollo Client provider in the root layout

---
+++ src/lib/apollo-client.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const GRAPHQL_API_URL =
+  process.env.NEXT_PUBLIC_GRAPHQL_API_URL ?? "https://rickandmortyapi.com/graphql";
+
+export const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHQL_API_URL }),
+  cache: new InMemoryCache(),
+});
