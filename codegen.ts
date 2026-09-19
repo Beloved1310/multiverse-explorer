e64098e232950a9501d1057c86f7468ec42a1252
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-09-19 13:57:17 +0100

    feat: configure GraphQL Code Generator against the Rick and Morty schema

---
+++ codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const GRAPHQL_API_URL =
+  process.env.NEXT_PUBLIC_GRAPHQL_API_URL ?? "https://rickandmortyapi.com/graphql";
+
+const config: CodegenConfig = {
+  schema: GRAPHQL_API_URL,
+  documents: ["src/**/*.graphql"],
+  ignoreNoDocuments: true,
+  generates: {
+    "src/gql/generated.ts": {
+      plugins: ["typescript", "typescript-operations", "typescript-react-apollo"],
+      config: {
+        withHooks: true,
+        avoidOptionals: true,
+      },
+    },
+  },
+};
+
+export default config;
